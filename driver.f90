765a1794f3c62bc21b2ea35e04394571e5512e7e
Shreyas911 <shreyasg911@gmail.com>
2020-10-23 14:02:49 -0700

    Uncommented FD which works fine. Only need to make tangent and reverse make together.

--- driver.f90
+++ driver.f90
@@ -18,32 +18,33 @@ program driver
 	V = 0.
 	call forward_problem_b(xx,xxb,V,Vb)
 	print *,xxb
-	! print *, "#    Reverse    FD    Tangent    Relative accuracy"
-	! !! Finite differences and Tangent Linear Model
-	! do ii = 1, nx+1
+	
+	print *, "#    Reverse    FD    Tangent    Relative accuracy"
+	!! Finite differences and Tangent Linear Model
+	do ii = 1, nx+1
 
-	! 	xx = 0.
-	! 	V = 0.
-	! 	xx_tlm = 0.
-	! 	xx_tlm(ii) = 1.
+		xx = 0.
+		V = 0.
+		xx_tlm = 0.
+		xx_tlm(ii) = 1.
 
-	! 	!! TLM
-	! 	call forward_problem_d(xx,xx_tlm,V,Vd)
+		!! TLM
+		! call forward_problem_d(xx,xx_tlm,V,Vd)
 
 
-	! 	!! FD
-	! 	xx = 0.
-	! 	V = 0.
-	! 	xx(ii) = eps
-	! 	call forward_problem(xx,V)
-	! 	xx_fd(ii) =  (V - V_forward)/eps
+		!! FD
+		xx = 0.
+		V = 0.
+		xx(ii) = eps
+		call forward_problem(xx,V)
+		xx_fd(ii) =  (V - V_forward)/eps
 
- !        if ( xx_fd(ii).NE. 0. ) then
- !            accuracy(ii) = 1.d0 - Vd/xx_fd(ii)
- !        else
- !            accuracy(ii) = 0.
- !        end if
- !        print *, ii, "    ", xxb(ii), "    ", xx_fd(ii),"    ", Vd,"    ", accuracy(ii)
-	! end do
+        if ( xx_fd(ii).NE. 0. ) then
+            accuracy(ii) = 1.d0 - Vd/xx_fd(ii)
+        else
+            accuracy(ii) = 0.
+        end if
+        print *, ii, "    ", xxb(ii), "    ", xx_fd(ii),"    ", Vd,"    ", accuracy(ii)
+	end do
 end program driver
 
